c6387643c03c371c583e2e3190cae19f81d9ef2b
Manasvi Saxena <msaxena2@illinois.edu>
2020-12-05 17:12:29 -0600

    g a -u ! WIP on renaming buitin includes

--- test.sh
+++ test.sh
@@ -3,16 +3,21 @@
 set -e
 
 runmaude()  {
-  maude -no-banner -no-wrap <(echo 'set show timing off .') "$@"
+  maude -no-banner -no-wrap </dev/null <(echo 'set show timing off .') "$@"
 }
 
 dodiff() {
   git --no-pager diff --no-index --word-diff "$@"
 }
 
+# sanity check
+runmaude p-semantics.maude
+
+exit
+
 for t in t/*.maude; do
   test_actual=$(mktemp -t tmp-tableaux-XXXXXX)
-  runmaude "$t" </dev/null &> "$test_actual"
+  runmaude "$t" &> "$test_actual"
   dodiff "$test_actual" "$t.expected"
 done
 
